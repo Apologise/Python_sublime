01d89336f12fc139462a47a863dd9e2294ff42c4
Apologise <603061018@qq.com>
2017-06-17 08:38:15 +0800

    第五章

---
+++ 第五章/5.3.py
@@ -0,0 +1,39 @@
+alien_color = 'green'
+if alien_color == 'green':
+	print('The player get 5 points')
+
+if alien_color == 'green':
+	print('The alien had get 5 points')
+else:
+	print('The alien had get 10 points')
+
+if alien_color == 'green':
+	print('The alien had get 5 point')
+elif alien_color == 'yellow':
+	print('The alien had get 10 point')
+elif alien_color == 'red':
+	print('The alien had get 15 point')
+
+age = 12
+if age < 2:
+	print('你是婴儿')
+elif age >= 2 and age < 4:
+	print('你正在蹒跚学步')
+elif age >= 4 and age < 13:
+	print('你是儿童')
+elif age >= 13 and age < 20:
+	print('你是青少年')
+elif age >= 20 and age < 65:
+	print('你是成年人')
+else:
+	print('你是老年人')
+
+favorite_fruits = ['apple', 'watermelon', 'orange']
+if 'apple' in favorite_fruits:
+	print('You really like bananas')
+if 'watermelon' in favorite_fruits:
+	print('You really like bananas')
+if 'orange' in favorite_fruits:
+	print('You really like bananas')
+if 'fuck' in favorite_fruits:
+	print('You really like fuck')
