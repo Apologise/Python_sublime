918e09d86c9ca3bda80fc85c2e67b93f52f19ec1
Apologise <603061018@qq.com>
2017-06-19 16:42:02 +0800

    第九章类

---
+++ 第九章/9-1餐馆.py
@@ -0,0 +1,18 @@
+class Restaurant():
+	"""模拟餐馆"""
+	def __init__(self, restaurant_name, cuisine_type):
+		"""初始化属性"""
+		self.restaurant_name = restaurant_name
+		self.cuisine_type = cuisine_type
+
+	def decribe_restaurant(self):
+		"""打印类的两个属性"""
+		print(self.restaurant_name+self.cuisine_type)
+
+	def open_restaurant(self):
+		"""显示餐馆的营业状态"""
+		print("餐馆正在营业")
+
+restaurant = Restaurant('每日一餐', '早餐')
+restaurant.decribe_restaurant()
+restaurant.open_restaurant()
