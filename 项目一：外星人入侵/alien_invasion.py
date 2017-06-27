3bb1deb1ff3194680b684d77b4907b6e2df33af0
Apologise <603061018@qq.com>
2017-06-27 21:29:59 +0800

    你好

--- 项目一：外星人入侵/alien_invasion.py
+++ 项目一：外星人入侵/alien_invasion.py
@@ -6,6 +6,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from alien import Alien
 
 def run_game():
 	#初始化游戏并创建一个屏幕对象
@@ -21,16 +22,23 @@ def run_game():
 	#创建一艘飞船
 	ship = Ship(screen, ai_settings)
 
+	#创建一个外星人实例
+	alien = Alien(ai_settings, screen)
+
 	#创建一个用于储存子弹的编组
 	bullets = Group()
+	#创建外星人群
+	aliens = Group()
+	gf.create_fleet(ai_settings, screen, ship,aliens)
 
 	#开始游戏的主循环
 	while True:
 		#监视键盘和鼠标事件
 		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update(ai_settings, screen, ship, bullets)
-		gf.update_bullets(bullets)
-		gf.update_screen(ai_settings, screen, ship, bullets)
+		gf.update_bullets(ai_settings, screen, ship,aliens,bullets)
+		gf.update_aliens(ai_settings,aliens)
+		gf.update_screen(ai_settings, screen, ship, aliens,bullets)
 
 
 run_game()
--- 项目一：外星人入侵/settings.py
+++ 项目一：外星人入侵/settings.py
@@ -13,3 +13,6 @@ class Settings():
 		self.bullet_color = (60,60,60)
 		self.bullet_speed_factor = 1
 		self.bullets_allowed = 10
+		self.alien_speed_factor = 0.6
+		self.fleet_drop_speed = 10
+		self.fleet_direction = 1
